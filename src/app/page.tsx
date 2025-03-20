2f607a69ab9f5c500d5542f7e5ac560cff86eac6
Suryansh Srivastava <107535408+suryansh-business-work@users.noreply.github.com>
2025-03-20 08:40:53 +0530

    Intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="content">
+          <div className="item">
+            <h1>MCP Server Wiki 🚀</h1>
+            <p>Unlock the future of AI with **Model Context Protocol (MCP)** servers.</p>
+            <p>Stay tuned for the ultimate **MCP server directory** – featuring real-time, dynamic, and scalable AI solutions!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
