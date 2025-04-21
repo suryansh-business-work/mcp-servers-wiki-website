1bd67947ebe0193395d475a398a08f1c11823464
Suryansh Srivastava <suryansh.business.work@gmail.com>
2025-04-21 09:03:02 +0530

    Nac Update

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './page.module.scss';
+import styles from './header.module.scss';
 
 export default function Header() {
   return (
@@ -12,6 +12,18 @@ export default function Header() {
           </li>
           <li className="item">
             <Link href={'/chat'}>Chat</Link>
+          </li>
+          <li className="item">
+            <Link href={'/integrations'}>Integrations</Link>
+          </li>
+          <li className="item">
+            <Link href={'/docs'}>Docs</Link>
+          </li>
+          <li className="item">
+            <Link href={'/docs'}>Download Local MCP Tunnel Tool</Link>
+          </li>
+          <li className="item cta">
+            <Link href={'/create-mcp-server'}>Create MCP Server</Link>
           </li>
         </ul>
       </div>
